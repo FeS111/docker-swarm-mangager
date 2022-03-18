9d82ed36dc325d1fa6a146aebf57891294b22778
Felix <felix.schmid@siriusonline.de>
2022-03-18 14:40:27 +0100

    service router setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,24 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import {config} from 'dotenv';
-import { getService, scaleService, syncServices } from './docker/service';
-config()
+import { config } from 'dotenv';
+import { syncServices } from './docker/service';
+import { router as serviceRouter } from './router/service.router';
+config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/service', serviceRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send('Something went wrong');
+});
+
 app.listen(process.env.PORT ?? 8080, async () => {
   console.log(`Server is listening on port "${process.env.PORT ?? 8080}"`);
   syncServices();
 });
-
-
---
+++ server/src/router/service.router.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { prisma } from '../controller/prisma.controller';
+import { updateService } from '../docker/service';
+import { DockerService } from '../intefaces/docker-service.interface';
+
+export const router = Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.send(await prisma.dockerService.findMany());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.send(
+      await prisma.dockerService.findFirst({
+        where: {
+          serviceID: { equals: req.params.id }
+        }
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/', async (req, res, next) => {
+  try {
+    res.send(await updateService(req.body as DockerService));
+  } catch (error) {
+    next(error);
+  }
+});
